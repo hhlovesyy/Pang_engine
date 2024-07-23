5e2638c098549fd59cbd36af792ab34f87ddb378
hhlovesyy <1596944152@qq.com>
2024-07-23 20:46:34 +0800

    实现了深度缓冲区

--- MiniRendererCpp/MiniRendererCpp/main.cpp
+++ MiniRendererCpp/MiniRendererCpp/main.cpp
@@ -8,9 +8,9 @@
 constexpr int width = 800; // output image size
 constexpr int height = 800;
 
-vec3 barycentric(vec2* pts, vec2 P) 
+vec3 barycentric(vec3* pts, vec3 P)
 {
-    vec3 u = cross(vec3(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) , vec3(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
+    vec3 u = cross(vec3(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]), vec3(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
     /* `pts` and `P` has integer value as coordinates
        so `abs(u[2])` < 1 means `u[2]` is 0, that means
        triangle is degenerate, in this case return something with negative coordinates */
@@ -18,7 +18,7 @@ vec3 barycentric(vec2* pts, vec2 P)
     return vec3(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
-void triangle(vec2 pts[3], TGAImage& image, TGAColor color)
+void triangle(vec3 pts[3], TGAImage& image, TGAColor color, std::vector<double>& zbuffer)
 {
     vec2 bboxmin(image.width() - 1, image.height() - 1);
     vec2 bboxmax(0, 0);
@@ -30,31 +30,37 @@ void triangle(vec2 pts[3], TGAImage& image, TGAColor color)
         bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
         bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
     }
-    vec2 P;
-    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) 
+    vec3 P;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
     {
-        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) 
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
         {
             vec3 bc_screen = barycentric(pts, P);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            image.set(P.x, P.y, color);
+            //添加与ZBuffer有关的逻辑判断
+            P.z = 0;
+            for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i]; //相当于对深度做重心插值
+            if (P.z < zbuffer[int(P.x + P.y * width)])
+            {
+                zbuffer[int(P.x + P.y * width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
         }
     }
-
 }
 
 //blue：TGAColor(255, 0, 0
 TGAColor red{ 255, 0, 0, 255, 4 };
 
-void RenderJustTriangle(TGAImage& framebuffer)
+void RenderJustTriangle(TGAImage& framebuffer, std::vector<double>& zbuffer)
 {
-    vec2 pts[3] = { vec2(10,10), vec2(700, 60), vec2(700, 560) };
-    triangle(pts, framebuffer, red);
+    vec3 pts[3] = { vec3(10,10, 0), vec3(700, 60, 0), vec3(700, 560, 0) };
+    triangle(pts, framebuffer, red, zbuffer);
     framebuffer.write_tga_file("simpleTriangle.tga");
 }
 
 vec3 light_dir(0, 0, -1);
-void RenderModel(TGAImage& framebuffer)
+void RenderModel(TGAImage& framebuffer, std::vector<double>& zbuffer)
 {
     //Model model("obj/african_head.obj"); // load an object
     Model model("obj/Robin.obj");
@@ -62,11 +68,11 @@ void RenderModel(TGAImage& framebuffer)
     {
         vec3 vert_pos[3];
         vec3 world_coords[3];
-        vec2 screen_coords[3];
+        vec3 screen_coords[3];
         for (int j : {0, 1, 2})
         {
             vert_pos[j] = model.vert(i, j);
-            screen_coords[j] = vec2(static_cast<int>((vert_pos[j].x + 1.) * width / 2.), static_cast<int>((vert_pos[j].y + 1.) * height / 2. - 270)); //暂时完全不考虑z的问题，也不考虑空间变换
+            screen_coords[j] = vec3(static_cast<int>((vert_pos[j].x + 1.) * width / 2.), static_cast<int>((vert_pos[j].y + 1.) * height / 2. - 290), -vert_pos[j].z * 10); //暂时不考虑空间变换
             world_coords[j] = vert_pos[j];
         }
         vec3 n = cross((world_coords[2] - world_coords[0]) , (world_coords[1] - world_coords[0]));
@@ -75,15 +81,17 @@ void RenderModel(TGAImage& framebuffer)
         if (intensity > 0) 
         {
             TGAColor color{ intensity * 255, intensity * 255, intensity * 255, 255, 4 };
-            triangle(screen_coords, framebuffer, color);
+            triangle(screen_coords, framebuffer, color, zbuffer);
         }
     }
+    //std::cout << zbuffer[0] << std::endl;
     framebuffer.write_tga_file("huangquanwu.tga");
 }
 
 int main(int argc, char** argv) {
     TGAImage framebuffer(width, height, TGAImage::RGB); // the output image
-    //RenderJustTriangle(framebuffer);
-    RenderModel(framebuffer);
+    std::vector<double> zbuffer(width * height, std::numeric_limits<double>::max());
+    //RenderJustTriangle(framebuffer, zbuffer);
+    RenderModel(framebuffer, zbuffer);
     return 0;
 }
