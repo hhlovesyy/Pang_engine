6b7a0eefe550c202c3413d51c3acbf518c7a3db9
hhlovesyy <1596944152@qq.com>
2024-07-28 16:11:50 +0800

    搞了一个片元着色器的背面剔除，fix bug

--- MiniRendererCpp/MiniRendererCpp/our_gl.cpp
+++ MiniRendererCpp/MiniRendererCpp/our_gl.cpp
@@ -44,10 +44,6 @@ void triangle(const vec4 clip_verts[3], IShader& shader, TGAImage& image, std::v
 {
     //clip_verts是NDC空间的三角形顶点坐标,还没有做透视除法
     
-	//先做窗口映射，再做透视除法，应该也没什么毛病
-	//vec4 pts[3] = { Viewport * clip_verts[0],    Viewport * clip_verts[1],    Viewport * clip_verts[2] };  // triangle screen coordinates before persp. division
-	//vec2 pts2[3] = { proj<2>(pts[0] / pts[0][3]), proj<2>(pts[1] / pts[1][3]), proj<2>(pts[2] / pts[2][3]) };  // triangle screen coordinates after  perps. division
-    
     //先做透视除法吧，然后再做视口变换
     vec4 pts[3] = { clip_verts[0] / clip_verts[0][3], clip_verts[1] / clip_verts[1][3], clip_verts[2] / clip_verts[2][3] };  // triangle screen coordinates before persp. division
     vec4 tmpPts2[3] = { Viewport * pts[0],    Viewport * pts[1],    Viewport * pts[2] };  // triangle screen coordinates after  perps. division
@@ -69,16 +65,28 @@ void triangle(const vec4 clip_verts[3], IShader& shader, TGAImage& image, std::v
 
             //refs:https://blog.csdn.net/Motarookie/article/details/124284471
             vec3 bc_screen = barycentric(pts2, { static_cast<double>(x), static_cast<double>(y) });
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+            
             //bc_screen 是屏幕空间计算出来的重心坐标，也就是链接中的α'，β'，γ'
-            //vec3 bc_clip = { bc_screen.x / pts[0][3], bc_screen.y / pts[1][3], bc_screen.z / pts[2][3] };
-            ////bc_clip.x + bc_clip.y + bc_clip.z 相当于1/z,见参考链接。z是屏幕空间的点P所对应的投影前的点的深度值，我们求解的目标值
-            //bc_clip = bc_clip / (bc_clip.x + bc_clip.y + bc_clip.z); 
             ////bc_clip相当于重新计算之后的正确的重心坐标，这里比较绕，一定要对应给的链接中的公式来看
             float w_reciprocal = 1.0 / (bc_screen.x / clip_verts[0][3] + bc_screen.y / clip_verts[1][3] + bc_screen.z / clip_verts[2][3]);
             // 但是这里又用了任意属性插值公式， 并且IA取的v[0].z()
             vec3  bc_clip = w_reciprocal * vec3(bc_screen.x / clip_verts[0][3] , bc_screen.y /clip_verts[1][3] , bc_screen.z / clip_verts[2][3]);
-            double frag_depth = vec3{ clip_verts[0][2], clip_verts[1][2], clip_verts[2][2] }*bc_clip;
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z<0 || frag_depth > zbuffer[x + y * image.width()]) continue;
+            //std::cout<< bc_clip.x << " " << bc_clip.y << " " << bc_clip.z << " "<< (bc_clip.x + bc_clip.y + bc_clip.z) << std::endl;
+            double frag_depth = vec3{ clip_verts[0][2], clip_verts[1][2], clip_verts[2][2] } * bc_clip;
+            bool shouldClipInFragment = false;  
+    //        for (int i = 0; i < 3; i++)  //trick:片元着色器暴力裁剪
+    //        {
+    //            //如果不在NDC空间范围内，就不渲染了
+    //            if (!(pts[i][0] >= -1 && pts[i][0] <= 1) || !(pts[i][1] >= -1 && pts[i][1] <= 1) || !(pts[i][2] >= -1 && pts[i][2] <= 1))
+    //            {
+    //                shouldClipInFragment = true;
+    //                break;
+				//}
+    //        }
+            //shouldClipInFragment = false;
+            if(shouldClipInFragment || frag_depth > zbuffer[x + y * image.width()]) continue;
+
             TGAColor color;
             if (shader.fragment(bc_clip, color, face_index)) continue; // fragment shader can discard current fragment
             zbuffer[x + y * image.width()] = frag_depth;
